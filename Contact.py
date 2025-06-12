f65acce6bc8ed679f428789777f7f379ab459769
Cinder798 <yangyemishi99@gmail.com>
2025-06-12 21:05:12 +0800

    Create Contact.py

---
+++ Contact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_title="Contact", layout="wide")
+st.title("📨 Contact Me")
+st.markdown("If you'd like to connect or collaborate, feel free to email me.")
+st.markdown("[📧 Click here to email](mailto:yangyemishi99@gmail.com)")
